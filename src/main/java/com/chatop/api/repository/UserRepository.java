6c660954a6d9a2eb30fc7b096eb9d75df4ddb791
Bhahlou <bhahlou@gmail.com>
2026-05-28 10:25:56 +0200

    Implement JWT security + swagger

--- src/main/java/com/chatop/api/repository/UserRepository.java
+++ src/main/java/com/chatop/api/repository/UserRepository.java
@@ -6,6 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    // Spring Data génère le SQL tout seul à partir du nom de la méthode
     Optional<User> findByEmail(String email);
 }
